a12daf55ec232434f48df38b8e689d2fa85c2323
WinsonLui <91889212+WinsonLui@users.noreply.github.com>
2022-09-05 11:48:41 +0800

    Create Game_log_analysis.py

---
+++ Game_log_analysis.py
@@ -0,0 +1,9 @@
+#Import packages
+import pandas
+
+#Extract gl data in all games across all seasons within the study period
+gl_raw = 
+
+#Rename the columns
+gl_raw_colnames = ['Date','Time','Visitor','Visitor PTS','Home','Home PTS','Box Score','OT','Attendance','Arena','Play-in Games']
+gl_raw.columns = gl_raw_colnames
